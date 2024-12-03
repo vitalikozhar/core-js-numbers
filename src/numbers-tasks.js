b734c79946984ee89f0d14388685953da6b60ac9
Vitali Kozhar <voxfree@pm.me>
2024-12-03 21:37:31 +0400

    feat: resolution

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -108,7 +108,7 @@ function getLinearEquationRoot(a, b) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(x1, y1, x2, y2) {
+function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
   throw new Error('Not implemented');
 }
 
